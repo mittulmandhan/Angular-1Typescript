7645b62d520381953ac6266005d603b3fbe79d21
mittulmandhan <conanmittul@gmail.com>
2019-09-05 23:52:10 +0530

    overloading

---
+++ overloading.ts
@@ -0,0 +1,8 @@
+//signatures
+function add(x: number, y: number, z: number): number;
+function add(x: string, y: string, z: string): string;
+
+// Implementation or u can say definition
+function add(x: any, y: any, z: any): any {
+
+}
