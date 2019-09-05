e5bee91c0bb047cb13c2a4f383465c00471efbbe
mittulmandhan <conanmittul@gmail.com>
2019-09-05 23:54:09 +0530

    overloading

--- overloading.ts
+++ overloading.ts
@@ -1,8 +1,18 @@
 //signatures
-function add(x: number, y: number, z: number): number;
-function add(x: string, y: string, z: string): string;
+function adding(x: number, y: number, z: number): number;
+function adding(x: string, y: string, z: string): string;
 
 // Implementation or u can say definition
-function add(x: any, y: any, z: any): any {
-
+function adding(x: any, y: any, z: any): any {
+    let result: any;
+    if(typeof x=="number" && typeof y=="number" && typeof z=="number") {
+        result = x + y + z;
+    }
+    else {
+        result = x + y + " " + z;
+    }
+    return result;
 }
+
+let result1 = add(4, 3, 8);//15
+let result2 = add("gurukul", "sight", "website");//gurukulsight website
