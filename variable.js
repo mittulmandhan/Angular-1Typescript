779f50d62cd40a33c0ed3719fe3fa1bc33de6d7c
mittulmandhan <conanmittul@gmail.com>
2019-09-05 22:50:19 +0530

    class1: Typescript basic compilation process

---
+++ variable.js
@@ -0,0 +1,10 @@
+if (true) {
+    var x = 2;
+    var y = 3;
+    var z = 3.14;
+    //cont and let can not be accessed outside if block
+    console.log(y);
+    console.log(z);
+}
+console.log(x);
+//console.log(y);
---
+++ variable.ts
@@ -0,0 +1,12 @@
+if(true) {
+    var x: number = 2;
+    let y: number = 3
+    const z: number = 3.14;
+
+    //cont and let can not be accessed outside if block
+    console.log(y);
+    console.log(z);
+}
+
+console.log(x);
+//console.log(y);
